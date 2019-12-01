a41bf54cbcc2e3da9cd0cb6f9f6c6fad622a17a7
Ribhararnus Pracutian <oknoorap@gmail.com>
2019-12-01 20:13:25 +0700

    fix: typo `basename`

--- src/utils/writer.ts
+++ src/utils/writer.ts
@@ -1,9 +1,9 @@
 import { writeFileSync } from "fs";
-import { basename } from "path";
+import { dirname } from "path";
 import mkdirp from "mkdirp";
 
 export default (path: string, status: string) => {
-  const dir = basename(path);
+  const dir = dirname(path);
   mkdirp.sync(dir);
   writeFileSync(path, status);
 };
